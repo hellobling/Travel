470c97b442a6561759344eda3948392cfba411e3
hellobling <623088099@qq.com>
2019-12-07 18:39:57 +0800

    city router

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,13 @@
     <span class="iconfont">&#xe632;</span>
       输入城市／景点／游玩主题
     </div>
-    <div class="header-right">
-      {{this.city}}
-      <span class="iconfont arrow-icon">&#xe64a;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont arrow-icon">&#xe64a;</span>
+      </div>
+    </router-link>
+
   </div>
 </template>
 <script>
@@ -25,8 +28,7 @@ export default {
   @import '~styles/varibles.styl'
   .header
     display: flex
-    height: .86rem
-    line-height: .86rem
+    line-height: $headerHeight
     background: $bgColor
     color: #fff
     .header-left
@@ -49,6 +51,7 @@ export default {
       width: 1.24rem
       float: right
       text-align: center
+      color: #fff
       .arrow-icon
         margin-left: -.04rem
         font-size: .24rem
